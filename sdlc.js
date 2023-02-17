1c68e34e9b2a2ae99c42b24954e0652f5cc94117
Seth Reid <reid.seth.96@gmail.com>
2023-02-17 12:02:15 -0700

    Initial commit

---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = 'Could also be first step, depending on who says it. Getting input from different teams and stakeholders to determine any requirements, such as needed resources or desired features\' feasability, or any potential legal issues.'
+let design = 'Third step, where developers come up with the general structure, or architecture, of any planned features or requirements. Also where the UI/UX design will be designed for devs to implement'
+let implementation = 'The actual building or coding of the project. Devs follow the plans and designs to ensure the product is built to the specs set out previously.'
+let testAndIntegrate = 'Testing is done on the project to ensure it works as designed and that there are minimal bugs and that none of them are critical. The different parts of the project are compiled together into a cohesive and user-focused application.'
+const maintenance = 'Maintenance is continued on the front and especially back-end of the applications to reduce bugs and keep the application running according to the initial designs.'
